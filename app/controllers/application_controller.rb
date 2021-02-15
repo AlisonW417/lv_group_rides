983b00fc2a5e01f0a7e7f66ce1fdd379370f3471
Alison Williams <apw5011@gmail.com>
2021-02-15 16:16:23 -0500

    set up files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,19 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    erb :home
   end
+
+  #helpers do 
+    #def current_user 
+      #User.find_by(id: session[:user_id])
+    #end 
+
+    #def redirect_if_not_logged_in 
+      #if !current_user 
+        #redirect '/login'
+      #end 
+    #end 
+  #end 
 
 end
